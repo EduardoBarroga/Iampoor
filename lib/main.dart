34ee6b4beb57f85a24d8b25bbec46ea5f6ad0a36
pc <jrbarroga05@gmail.com>
2022-12-14 12:19:30 -0800

    iampoor

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('I Am Poor'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image:
+            AssetImage('images/rocks.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
